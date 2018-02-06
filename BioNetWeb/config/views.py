92469957a11309c317f261047c2072eae43314b3
JoshGutman <jng86@nau.edu>
2018-02-06 16:52:46 -0700

    Add test stuff

--- BioNetWeb/config/views.py
+++ BioNetWeb/config/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def create(request):
-    return render(request, 'config/create.html')
+    return render(request, 'config/create.html', {'observables': ['test1', 'test2']})
