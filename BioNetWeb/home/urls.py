0fd25a79477a80b430bd231c9e3aecdc8ec89ad6
burninggiraffe <mrb375@nau.edu>
2018-02-16 09:57:19 -0700

    create framework
    Add frame pages to site

--- BioNetWeb/home/urls.py
+++ BioNetWeb/home/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('login', views.login, name='login'),
+    path('feedback', views.feedback, name='feedback'),
+    path('resources', views.resources, name='resources'),
+    path('user', views.user, name='user'),
+    path('admin', views.admin, name='admin'),
 ]
--- BioNetWeb/home/views.py
+++ BioNetWeb/home/views.py
@@ -4,9 +4,20 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'home/index.html')
 
-
 def about(request):
     return render(request, 'home/about.html')
 
 def login(request):
     return render(request, 'home/login.html')
+
+def feedback(request):
+    return render(request, 'home/feedback.html')
+
+def resources(request):
+    return render(request, 'home/resources.html')
+
+def user(request):
+    return render(request, 'home/user.html')
+
+def admin(request):
+    return render(request, 'home/admin.html')
