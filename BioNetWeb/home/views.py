98b093552773a4448570ba0ea141f97ddc2b9d4d
blitzerama <tgb43@nau.edu>
2018-03-06 18:41:48 -0700

    Modified DB testing functionality

--- BioNetWeb/home/views.py
+++ BioNetWeb/home/views.py
@@ -38,38 +38,62 @@ def resources(request):
     return render(request, 'home/resources.html')
 
 def user(request):
+    return render(request, 'home/user.html')
 
-    ###### MONGO DB FUNCTIONALITY #####
-    # Note from Tanner: Hey, so I will label
-    # and implement a number of mongo db fucntionalities here.
+###### MONGO DB FUNCTIONALITY #####
+# Note from Tanner: Hey, so I will label
+# and implement a number of mongo db
+# fucntionalities here.
 
-    # First we generate a mongo connection
-    # We access the db BioNetFit, and
-    # the collection users.
+# First we generate a mongo connection
+# We access the db BioNetFit, and
+# the collection users.
+def establish_db_connect():
+
     client = MongoClient()
     db = client.BioNetFit
     users = db.users
+    return users
 
-    # Lets Start by Adding the User to the
-    # MongoDB. This accesses the request from
-    # user(request) and strips the username.
+
+# Lets Start by Adding the User to the
+# MongoDB. This accesses the request from
+# user(request) and strips the username.
+def add_db_user(request):
+
+    users = establish_db_connect()
     username = request.user.username
+
     new_user = {
         "user":username
     }
     users.insert(new_user)
+    return new_user
 
-    # Lets also add 2 files to the db
-    # under the specific username
+# Lets also add 2 files to the db
+# under the specific username
+# Fucntion used only for testing
+def add_files(request):
+
+    users = establish_db_connect()
+    username = request.user.username
+
     users.update({"user":username}, {'$set':{"file1":
-                                "This is a test file, named"
-                                " file1!"}})
+                    "This is a test file, named"
+                    " file1!"}})
     users.update({"user":username}, {'$set':{"file2":
-                                "This is a test file, named"
-                                " file2!"}})
+                    "This is a test file, named"
+                    " file2!"}})
 
-    # Now that we have this, lets pull those
-    # files out and print them to the console.
+
+# Now that we have this, lets pull those
+# files out and print them to the console.
+# Fucntion used for testing, can be modified
+def display_and_download(request):
+
+    users = establish_db_connect()
+    username = request.user.username
+
     for user in users.find({"user": username}):
         for field in user:
             if field != "user" and field != "_id":
@@ -81,16 +105,20 @@ def user(request):
                 with open(field, 'a') as file:
                     file.write(user[field])
 
-    # Well thats pretty sweet, but can
-    # we write from an existing file
-    # to the db? I don't see why not!
-    filename = "file3"
+# Well thats pretty sweet, but can
+# we write from an existing file
+# to the db? I don't see why not!
+def file_insert(request, filename):
+
+    users = establish_db_connect()
+    username = request.user.username
+
     # Avoid This, just for testing
     f = open(filename)
     text = f.read()
     users.update({"user": username}, {'$set': {filename: text}})
 
-    return render(request, 'home/user.html')
+### END DB TESTING FUNCTIONS ###
 
 def admin(request):
     return render(request, 'home/admin.html')
